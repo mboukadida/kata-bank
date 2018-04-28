a6b47e3dc35b27d30f15527185cfe2480bc2ebdb
Majed BOUKADIDA <majed.boukadida@gmail.com>
2018-04-29 00:10:10 +0200

    First version

---
+++ Kata.Bank.Domain/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Kata.Bank.Domain
+{
+    public class Account
+    {
+        private Guid accountId;
+        private double balance;
+        private string currency;
+        private Guid guid;
+
+        public static Account Empty()
+        {
+            return new Account(Guid.NewGuid(), 0, "EUR");
+        }
+
+        public static Account With(double balance)
+        {
+            return new Account(Guid.NewGuid(), balance);
+        }
+
+        private Account(Guid accountId, double balance, string currency)
+            : this(accountId, balance)
+        {
+            if (string.IsNullOrEmpty(currency)) throw new ArgumentNullException();
+        }
+
+        public Account(Guid guid, double balance)
+        {
+            this.guid = guid;
+            this.balance = balance;
+        }
+
+        private Account()
+        {
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount <= 0)
+                throw new Exception("A deposit must be positive");
+            balance += amount;
+        }
+
+        public void Debit(double amount)
+        {
+            var tempBalance = balance + amount;
+            if (tempBalance <= 0)
+                throw new Exception("You're not authorized to debit your account");
+
+            balance = tempBalance;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
